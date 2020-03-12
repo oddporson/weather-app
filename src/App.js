9caf0b368d3e5a0f317ba539740e6514283fde1d
Porson <porsonlee@gmail.com>
2020-03-11 20:19:59 -0700

    capitalized Celsius

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ function App() {
           </div>
           <div className="weather-box">
             <div className="temp">
-            {Math.round(weather.main.temp)}°c
+            {Math.round(weather.main.temp)}°C
             </div>
             <div className="weather">{weather.weather[0].main}</div>
           </div>
