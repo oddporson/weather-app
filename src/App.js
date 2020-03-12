5041a9ba8a3b9fb1006f5ec2be97399411245143
Porson <porsonlee@gmail.com>
2020-03-11 19:39:32 -0700

    set up date function

--- src/App.js
+++ src/App.js
@@ -6,6 +6,20 @@ const api = {
 }
 
 function App() {
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
+
   return (
     <div className="app">
       <main>
@@ -15,6 +29,17 @@ function App() {
             className="search-bar"
             placeholder="Search..."
           />
+        </div>
+        <div>
+          <div className="location-box"></div>
+            <div className="location">Vancouver, BC, Canada</div>
+            <div className="date">{dateBuilder(new Date())}</div>
+        </div>
+        <div className="weather-box">
+          <div className="temp">
+            15°c
+          </div>
+          <div className="weather">Sunny</div>
         </div>
       </main>
     </div>
