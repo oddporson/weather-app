911079a167c11afcd8ebd85ba7e48e0f71b2e407
Porson <porsonlee@gmail.com>
2020-03-11 20:10:35 -0700

    set conditioning where background image changes if weather if over 16 degree celsius

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
 
 
   return (
-    <div className="app">
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16)? 'app warm' : 'app') : 'app'}>
       <main>
         <div className="search-box">
           <input
