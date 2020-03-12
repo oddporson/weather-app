87c7c76e16f150a41712344e2956372b73c0e226
Porson <porsonlee@gmail.com>
2020-03-11 19:30:30 -0700

    styled the weather app

--- src/App.js
+++ src/App.js
@@ -7,8 +7,16 @@ const api = {
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world!</h1>
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+          />
+        </div>
+      </main>
     </div>
   );
 }
